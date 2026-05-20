52794504be039e0e7a15d46988d3a5357fa8f676
Blake Zeiger <blakezeiger@gmail.com>
2026-05-20 10:37:24 -0700

    theme

--- src/app/(auth)/index.tsx
+++ src/app/(auth)/index.tsx
@@ -1,10 +1,5 @@
 import LoginScreen from '@/features/auth/screens/LoginScreen'
-import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Login() {
-  return (
-    <SafeAreaView>
-      <LoginScreen />
-    </SafeAreaView>
-  )
+  return <LoginScreen />
 }
--- src/features/auth/screens/LoginScreen.tsx
+++ src/features/auth/screens/LoginScreen.tsx
@@ -1,16 +1,35 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 import { Text } from '@/components/ui/text'
 import { View } from 'react-native'
 
 const LoginScreen = () => {
   return (
-    <View className="h-full justify-center p-10 gap-6">
-      <Text>LoginScreen</Text>
-      <Input placeholder="Email" />
-      <Input placeholder="Password" secureTextEntry />
+    <View className="bg-background h-full justify-center p-6 gap-6">
+      <View>
+        <Text className="text-3xl font-bold">Welcome back</Text>
+        <Text className="text-sm text-muted-foreground">
+          Sign in to claim, defend, and compete.
+        </Text>
+      </View>
+
+      <View>
+        <Label nativeID="email-input" htmlFor="email-input">
+          Email
+        </Label>
+        <Input id="email-input" placeholder="Email" />
+      </View>
+
+      <View>
+        <Label nativeID="password-input" htmlFor="password-input">
+          Password
+        </Label>
+        <Input id="password-input" placeholder="Password" secureTextEntry />
+      </View>
+
       <Button onPress={() => {}}>
-        <Text>Login</Text>
+        <Text>Sign in</Text>
       </Button>
     </View>
   )
